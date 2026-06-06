87d18187051c742c54107dc940f9686953f18c8e
WixTheWolf <matt@flavorfactory.net>
2026-06-06 13:52:05 -0700

    Tighten robots rules for commerce SEO

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,12 @@
 import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  return { rules: { userAgent: '*', allow: '/' }, sitemap: 'https://wyxgolfsupply.com/sitemap.xml' };
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/', '/cart', '/checkout', '/checkouts']
+    },
+    sitemap: 'https://wyxgolfsupply.com/sitemap.xml'
+  };
 }
